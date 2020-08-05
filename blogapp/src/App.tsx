18ae02a0b9a8fba3697ab760a2a6376dc4fc112a
ArdenMendoza <ardencristophermendoza@gmail.com>
2020-08-05 23:46:14 +0800

    Added Post New Blog Dialog

--- blogapp/src/App.tsx
+++ blogapp/src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import './App.css';
-import { Input, Button, Row, Col } from 'antd';
+import { Input, Button, Row, Col, Modal } from 'antd';
 import { BlogPostView } from './components/blogPostView';
 import { BlogListView } from './components/blogListView';
-import { EditBlogPostView } from './components/editBlogView'
+import { EditBlogPostView } from './components/editBlogView';
 import { FileAddOutlined } from '@ant-design/icons';
 import { useSelector, connect } from 'react-redux';
 import { BlogAppState } from './store/reducer/postReducer';
-import { searchBlog } from './store/actions/blogActions'
+import { searchBlog } from './store/actions/blogActions';
+import { BookOutlined } from '@ant-design/icons';
 
-const { Search } = Input;
+const { Search, TextArea } = Input;
 
 interface ReduxStateProps {
   searchTerm?: string;
@@ -17,22 +18,49 @@ interface ReduxStateProps {
 }
 interface DispatchProps {
   onSearch: (searchText: string) => void;
+  onPostBlog: (title: string, content: string) => void;
 }
 
-type Props = {
-
-}
+type Props = {}
 
 const customStyles = {
   spacing: {
     marginRight: '8px',
     marginBottom: '12px',
     width: '300px'
+  } as React.CSSProperties,
+  textboxSpacing: {
+    marginRight: '8px',
+    marginBottom: '12px'
   } as React.CSSProperties
 }
 
 const AppDump: React.StatelessComponent<Props & ReduxStateProps & DispatchProps> = (props) => {
-  const { onSearch, isOnEditMode } = props;
+  const { onSearch, onPostBlog, isOnEditMode } = props;
+
+  const modalState = {
+    visible: false,
+    confirmLoading: false,
+  };
+
+  const [mState, setMState] = React.useState(modalState);
+  const [title, setTitle] = React.useState('');
+  const [content, setContent] = React.useState('');
+
+  const postBlog = () => {
+    // call action to send title, content to server
+    onPostBlog(title, content);
+    // hide dialog and reset values
+    resetDialog();
+  }
+
+  const resetDialog = () => {
+    // hide dialog and reset values
+    setMState({ ...mState, visible: false });
+    setTitle('');
+    setContent('');
+  }
+
   return (
     <div className="App">
 
@@ -44,7 +72,7 @@ const AppDump: React.StatelessComponent<Props & ReduxStateProps & DispatchProps>
             onChange={e => onSearch(e.currentTarget.value)}
             style={customStyles.spacing}
           />
-          <Button type="primary" icon={<FileAddOutlined />} size={'middle'} />
+          <Button type="primary" icon={<FileAddOutlined />} size={'middle'} onClick={() => setMState({ ...mState, visible: true })} >{'Create New Blog'}</Button>
           <BlogListView />
         </Col>
         <Col span={12}>
@@ -53,6 +81,17 @@ const AppDump: React.StatelessComponent<Props & ReduxStateProps & DispatchProps>
           }
         </Col>
       </Row>
+      <Modal
+        title="Create New Blog"
+        visible={mState.visible}
+        onOk={postBlog}
+        okText={'Post'}
+        onCancel={resetDialog}
+        cancelText={'Cancel'}
+        confirmLoading={mState.confirmLoading}>
+        <Input style={customStyles.textboxSpacing} size="large" placeholder="Title" prefix={<BookOutlined />} value={title} onChange={e => setTitle(e.currentTarget.value)} />
+        <TextArea style={customStyles.textboxSpacing} rows={20} value={content} onChange={e => setContent(e.currentTarget.value)} />
+      </Modal>
     </div >
   );
 }
@@ -61,7 +100,8 @@ export const App = connect<ReduxStateProps, DispatchProps, {}, BlogAppState>((st
   searchTerm: state.searchTerm,
   isOnEditMode: state.isOnEditMode,
 }), dispatch => ({
-  onSearch: (searchText: string) => dispatch(searchBlog(searchText))
+  onSearch: (searchText: string) => dispatch(searchBlog(searchText)),
+  onPostBlog: (title: string, content: string) => console.log(title, content)
 }))(AppDump);
 
 export default App;
